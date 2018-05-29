9bcf1221bf0c89d0dc74c8128486bc97a377ff2b
chenrui <chenrui@boxiao.cn>
2018-05-29 16:54:23 +0800

    地产以吃卡卡

---
+++ MyAppTest/MyAppTest/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  MyAppTest
+//
+//  Created by chenrui on 2018/5/29.
+//  Copyright © 2018年 博校. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"dwdwklwkdk");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
